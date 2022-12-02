6133d5b83d109949ca439c198e9e0aefc647d2de
Ipedis-technicalTeam <technical-team@ipedis.com>
2022-12-02 16:55:26 +0400

    First Commit

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import './assets/sass/index.scss';
+import './js/skipLinks';
+import './js/translation';
+
+// Only For Lang Switch Btn Demo
+import './js/switchLang';
---
+++ webpack.dev.js
@@ -0,0 +1,70 @@
+const path = require('path');
+const { merge } = require('webpack-merge');
+const common = require('./webpack.common.js');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const htmlPartialsPageNames = require('./src/partials').htmlPartialsPageNames;
+const sectionPartials = require('./src/partials/sections').sectionPartials;
+
+let multipleSectionPlugins = sectionPartials.map(name => {
+  return new HtmlWebpackPlugin({
+    template: path.join(__dirname, `./src/partials/sections/${name}.html`),
+    filename: `${name}.html`,
+  });
+});
+
+let multipleHtmlPlugins = htmlPartialsPageNames.map(name => {
+  return new HtmlWebpackPlugin({
+    template: path.join(__dirname, `./src/partials/${name}.html`),
+    filename: `${name}.html`,
+  });
+});
+
+module.exports = merge(common, {
+  mode: 'development',
+
+  devtool: 'source-map',
+
+  devServer: {
+    static: {
+      directory: path.join(__dirname, './src'),
+    },
+    port: 3000,
+  },
+
+  module: {
+    rules: [
+      // SASS LOADER
+      {
+        test: /\.scss$/i,
+        use: [
+          'style-loader',
+          'css-loader',
+          {
+            loader: 'sass-loader',
+            options: {
+              implementation: require('sass'),
+            },
+          },
+        ],
+      },
+    ],
+  },
+
+  plugins: [
+    // HTML
+    new HtmlWebpackPlugin({
+      template: path.join(__dirname, './src/index.html'),
+      filename: 'index.html',
+
+      // ********* TO CHANGE TITLE FOR DEV MODE ********* //
+      title: 'Development',
+
+      // ********* TO CHANGE LANG FOR DEV MODE ********* //
+      lang: 'fr',
+    }),
+
+    ...multipleHtmlPlugins,
+
+    ...multipleSectionPlugins,
+  ],
+});
