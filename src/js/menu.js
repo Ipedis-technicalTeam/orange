7feed4414ba43f4f6020967218f7056a7c05f9a1
akhileshsimothy <akhilesh@ipedis.com>
2022-12-19 10:39:14 +0400

    lp-akhilesh modif 19/12/22

--- src/js/menu.js
+++ src/js/menu.js
@@ -1,16 +1,28 @@
 let toggleMenuBtn = document.querySelector('.menu_button');
 let menu = document.querySelector('#menu');
 let menuItems = menu.querySelectorAll('.menu__list__item-link');
+var current = document.getElementsByClassName("active");
+
 
 for (var i = 0; i < menuItems.length; i++) {
     menuItems[i].addEventListener("click", function() {
-        var current = document.getElementsByClassName("active");
         if (current.length > 0) {
             current[0].className = current[0].className.replace(" active", "");
         }
         this.className += " active";
     })
 }
+
+function callback(entries) {
+    entries.map((entry) => {
+        console.log("this is " + entry.target.id);
+    });
+}
+
+menuItems.forEach((target) => {
+    new IntersectionObserver(callback).observe(target);
+});
+
 
 function toggleTabindex(value) {
     menuItems.forEach((item) => item.setAttribute('tabindex', value));
@@ -33,36 +45,42 @@ function toggleMenu(e) {
     }
 
     let ariaAttribute = toggleMenuBtn.getAttribute('aria-expanded');
-    if (ariaAttribute === 'false') {
-        toggleMenuBtn.setAttribute('aria-expanded', 'true');
-        toggleTabindex(0);
-    } else {
-        toggleMenuBtn.setAttribute('aria-expanded', 'false');
-        toggleTabindex(-1);
+    if (window.screen.width <= 1024) {
+        if (ariaAttribute === 'false') {
+            toggleMenuBtn.setAttribute('aria-expanded', 'true');
+            toggleTabindex(0);
+        } else {
+            toggleMenuBtn.setAttribute('aria-expanded', 'false');
+            toggleTabindex(-1);
+        }
     }
 }
 
 toggleMenuBtn.addEventListener('click', toggleMenu);
 
 // On small screens, hide menu items from tab order until menu opens
-if (window.innerWidth < 900) {
+if (window.screen.width < 900) {
     toggleTabindex(-1);
 }
 
+
 let lastLink = document.querySelector('#lastLink');
-lastLink.addEventListener('keydown', function(e) {
-    if (e.keyCode === 9 && e.keyCode === 16 ) {
-        if (document.activeElement === toggleMenuBtn) {
-            lastLink.focus();
-            e.preventDefault();
+if (window.screen.width <= 1024) {
+    lastLink.addEventListener('keydown', function(e) {
+        if (e.keyCode === 9 && e.keyCode === 16 ) {
+            if (document.activeElement === toggleMenuBtn) {
+                lastLink.focus();
+                e.preventDefault();
+            }
+        } else if (e.keyCode === 9) {
+            if (document.activeElement === lastLink) {
+                toggleMenuBtn.focus();
+                e.preventDefault();
+            }
         }
-    } else if (e.keyCode === 9) {
-        if (document.activeElement === lastLink) {
-            toggleMenuBtn.focus();
-            e.preventDefault();
-        }
-    }
-});
+    });
+}
+
 
 var navHeight = document.querySelector('.lp-nav').offsetHeight;
 document.documentElement.style.setProperty('--scroll-padding', navHeight - 1 + 'px');
